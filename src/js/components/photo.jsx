03fd7d35ffca64e4f80b2189508839d82ddcfee2
montecamo <merrychristek@gmail.com>
2018-08-05 19:28:19 +0300

    change pure photos to photo-cards

--- src/js/components/photo.jsx
+++ src/js/components/photo.jsx
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
+import { Card, Icon } from 'antd'
+
+const { Meta } = Card;
 
 const Photo = (props) => (
-  <img onLoad={props.loadHandler} src={props.src} />
+  <Card
+    hoverable
+    style={{width: 240}}
+    cover={<img onLoad={props.loadHandler} src={props.src} />}
+  >
+    <Meta
+      title=''
+    />
+  </Card>
 );
+
+//TODO add hashtags on uploading
 
 export default Photo;
