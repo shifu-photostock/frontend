c51fd5d9615692daacecd62657f45d7afb104f32
montecamo <merrychristek@gmail.com>
2018-08-07 14:13:50 +0300

    add zoom image on click

--- src/js/components/photo.jsx
+++ src/js/components/photo.jsx
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
-import { Card, Icon } from 'antd'
+import { Card, Icon, Modal } from 'antd'
 
 import { deletePhoto } from '../actions/photosActions';
 
@@ -10,8 +10,15 @@ const { Meta } = Card;
 export default class Photo extends Component {
   constructor() {
     super();
+
+    this.state = {
+      visible: false
+    }
     
     this.handleClick = this.handleClick.bind(this);
+    this.showModal = this.showModal.bind(this);
+    this.handleOk = this.handleOk.bind(this);
+    this.handleCancel = this.handleCancel.bind(this);
   }
 
   handleClick(e) {
@@ -23,15 +30,49 @@ export default class Photo extends Component {
     }
   }
 
+  showModal() {
+    this.setState({
+      visible: true
+    })
+  }
+
+  handleOk() {
+    this.setState({
+      visible: false
+    })
+  }
+
+  handleCancel() {
+    this.setState({
+      visible: false
+    })
+  }
+
   render() {
     return (
-      <Card
-        hoverable
-        style={{width: 240}}
-        cover={<img onLoad={this.props.loadHandler} src={this.props.src} />}
-        actions={[<Icon onClick={this.handleClick} id='delete' type="close-circle-o" />,]}
-      >
-      </Card>
+      <Fragment>
+        <Card
+          hoverable
+          style={{width: 240}}
+          cover={<img onClick={this.showModal} onLoad={this.props.loadHandler} src={this.props.src} />}
+          actions={[<Icon onClick={this.handleClick} id='delete' type="close-circle-o" />,]}
+        >
+        </Card>
+        <Modal 
+          className='photo-modal'
+          visible={this.state.visible}
+          bodyStyle={{padding: '10px'}}
+          footer={null}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+          width='auto'
+          centered={true}
+        >
+          <img onLoad={this.props.loadHandler} src={this.props.src} />
+        </Modal>
+
+      </Fragment>
+
     )
   }
 };
